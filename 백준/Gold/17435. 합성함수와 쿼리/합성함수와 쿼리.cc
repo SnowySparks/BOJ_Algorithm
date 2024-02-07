dca27a4b5978fd3d73878570da6e899ae3a5949e
SnowySparks <156272562+SnowySparks@users.noreply.github.com>
2024-02-07 13:56:04 +0900

    [Gold I] Title: 합성함수와 쿼리, Time: 208 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Gold/17435. 합성함수와 쿼리/합성함수와 쿼리.cc
+++ 백준/Gold/17435. 합성함수와 쿼리/합성함수와 쿼리.cc
@@ -3,7 +3,7 @@
 #include <cstring>
 using namespace std;
 int n;
-int func[200001][22];
+int func[200001][20]; // i, lv : i -> input(x)에 대한 것, lv : f^( (1 << lv) )(x) 다루기 위한 인덱스
 
 int query(int lv, int x) {
     for (int i = 0 ; lv ; ++i) {
@@ -18,8 +18,9 @@ int main(void) {
     int q;
     cin >> n;
 
+    //f^4(n) == f^2 ( f^2(n) ) 같은 원리를 이용함
     for (int i = 1; i <= n; ++i) cin >> func[i][0];
-    for (int lv = 1; lv <= 21; ++lv) {
+    for (int lv = 1; lv <= 19; ++lv) {
         for (int i = 1;  i <= n ; ++i) {
             func[i][lv] = func[ func[i][lv-1] ][lv-1];
         }
