95c478139fd5c1346a988ba5f55f04b35190b410
SnowySparks <156272562+SnowySparks@users.noreply.github.com>
2024-02-10 01:33:42 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 390 ms, Memory: 13,700 KB -BaekjoonHub

--- SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.cpp
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.cpp
@@ -1,125 +1,88 @@
-#include <iostream>
-#include <cstring>
-#include <vector>
-#include <deque>
-#include <queue>
-using namespace std;
-#define f(a,b,c) for(int a=b; a<c;++a)
-int N, W, H; //구슬 갯수, 너비(열개수), 높이(행개수)
-int ans; //정답 -> 가능한 최소, 초깃세팅 : W * H
+from collections import deque
+from copy import deepcopy
+ans = 0
+def left_block(lst) -> int:
+    #입력받은 남은 블럭들의 개수 카운팅
+    cnt = 0
+    for row in range(H):
+        for col in range(W):
+            if lst[row][col] : cnt+=1
+    return cnt
 
-struct info {
-    int r, c, d;
-};
+def move_blocks(lst) :
+    # lst에 담긴 블럭들을 전부 이동
+    # deque를 이용(혹은 스택) 통해 쉽게 전부 아래로 이동 가능
+    dq = deque()
+    for col in range(W):
+        for row in range(H):
+            if lst[row][col]:
+                dq.append(lst[row][col])
+                lst[row][col] = 0
+        for row in range(H-1,-1,-1):
+            if not dq: break
+            lst[row][col] = dq.pop()
 
-//남은 블럭 갯수 카운팅
-int left_block(const vector<vector<int> > &dt) {
-    int res = 0;
-    f(i,0,H) {
-        f(j, 0, W) {
-            if (dt[i][j]) ++res;
-        }
-    }
-    return res;
-}
-
-//입력받은 블록 정보에 대해서 모든 블럭을 아래로 이동
-void move_blocks(vector<vector<int> > &dt) {
-    deque<int> dq;
-    f(col, 0, W) {
-        f(row, 0, H) {
-            if (dt[row][col]) dq.push_back(dt[row][col]);
-            dt[row][col] = 0;
-        }
-        int idx = H-1;
-        while (!dq.empty()) {
-            dt[idx][col] = dq.back();
-            dq.pop_back();
-            --idx;
-        }
-    }
-}
-
-//해당 선택된 열에 공 떨굴때의 시물레이션
-void delete_block(int idx, vector<vector<int> > &dt) {
-    int sr, sc = idx;
-    //가장 위 블록을 찾기
-    f(row, 0, H) {
-        if (dt[row][idx]) {
-            sr = row;
-            break;
-        }
-    }
-    //bfs 탐색
-    queue<info> q;
-    q.push({sr, sc, dt[sr][sc]});
-    while (!q.empty()) {
-        int r = q.front().r, c = q.front().c, d = q.front().d - 1; q.pop();
-        dt[r][c] = 0;
-        if (d == 0) continue;
-        for (int row = max(r-d, 0); row <= min(r+d,H-1); ++row) {
-            if (dt[row][c]) {
-                q.push({row, c, dt[row][c]});
-                dt[row][c] = 0;
-            }
-            else continue;
-        }
-        for (int col = max(c-d, 0); col <= min(c+d,W-1); ++col) {
-            if (dt[r][col]) {
-                q.push({r, col, dt[r][col]});
-                dt[r][col] = 0;
-            }
-            else continue;
-        }
-    }
-    move_blocks(dt);
-}
-
-void dfs(vector<vector<int> > dt,int cnt) { //dfs
-    if (cnt == N) {
-        ans = min(ans, left_block(dt));
-        return;
-    }
+def delete_block(loc : int, lst : list) :
+    #loc 열에 공을 떨굴 경우의 시물레이션 함수
+    sr = 0 #처음 블록이 있는 위치 탐색
+    for r in range(H):
+        if lst[r][loc]:
+            sr = r
+            break
+    #bfs simulation
+    #tuple형태로 주입: 행, 열, 범위
+    dq = deque()
+    dq.append((sr, loc, lst[sr][loc],))
+    while dq:
+        r, c, d = dq.popleft()
+        d-=1
+        lst[r][c]=0
+        
+        for row in range(max(r-d, 0), min(r + d, H-1) + 1):
+            if lst[row][c]:
+                dq.append((row, c, lst[row][c]))
+                lst[row][c] = 0
+        for col in range(max(c-d, 0), min(c + d, W-1) + 1):
+            if lst[r][col]:
+                dq.append((r, col, lst[r][col]))
+                lst[r][col] = 0
+    move_blocks(lst)
     
-    //만약 모든 블럭이 비어 있는 경우는 조기 종료
-    int left_col = 0;
-    for (int col = 0; col < W; ++col) {
-        if (dt[H-1][col]) ++left_col;
-    }
-    if (left_col == 0) {
-        ans = 0;
-        return;
-    }
-    //백업용
-    vector<vector<int> > cp(H, vector<int>(W, 0));
-    copy(dt.begin(), dt.end(), cp.begin());
+def dfs(cnt : int, lst : list):
+    global ans
+    #모든 공 투척에 대한 경우들 백트래킹
+    #cnt : 공 투척 횟수
+    #lst : 현재 입력받은 리스트 정보. 
+    if cnt == N:
+        ans = min(ans, left_block(lst))
+        return
+    
+    #조기종료 -> 블록이 하나도 없는경우. 가장 맨 바닥만 탐색
+    zero = True
+    for col in range(W):
+        if lst[H-1][col]:
+            zero = False
+            break
+    if zero:
+        ans = 0
+        return
+    
+    #백트래킹용 백업용 리스트
+    backup = deepcopy(lst)
+    
+    #공을 쏘는데 있어서 빈 공간을 쏠 필요 없음
+    for col in range(0, W):
+        if (lst[H-1][col] == 0): continue    
+        delete_block(col, lst)
+        dfs(cnt+1, lst)
+        lst = deepcopy(backup)
 
-    for (int col = 0; col < W; ++col) {
-        if (!dt[H-1][col]) continue; //해당 열에 더이상 블럭이 없으면 안떠구는게 무조건 이득
-        delete_block(col, dt); //해당 열에 공떨구기
-        dfs(dt, cnt+1);
-        copy(cp.begin(), cp.end(), dt.begin()); //롤 백
-    }
-}
 
-int main(void) {
-    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    int T; cin >> T;
-    for (int tc = 1; tc <= T; ++tc) {
-        cin >> N >> W >> H;
-        vector<vector<int> > vec(H, vector<int>(W)); //블록 정보들
-        for (int i = 0 ; i < H; ++i) {
-            for (int j = 0 ; j < W; ++j) {
-                cin >> vec[i][j];
-            }
-        }
-        ans = left_block(vec);
-        dfs(vec, 0);
-        // f(i, 0, H) {
-        //     f(j, 0, W) cout << vec[i][j] <<' ';
-        //     cout << '\n';
-        // }
-        cout << '#' << tc << ' ' << ans << '\n';
-    }
-    return 0;
-}
+T = int(input())
+for tc in range(1, T+1):
+    #input
+    N, W, H = map(int,input().split()) #공 호출수, 너비(열), 높이(행)
+    lst = [list(map(int,input().split())) for _ in range(H)]
+    ans = left_block(lst) #초기설정
+    dfs(0, lst)
+    print(f'#{tc} {ans}')
