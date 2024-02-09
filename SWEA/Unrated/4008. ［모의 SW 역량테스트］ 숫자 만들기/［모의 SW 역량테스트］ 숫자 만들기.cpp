4fdbd14a9ce12e7d9f2e6daed40ba07e7707c9f6
SnowySparks <156272562+SnowySparks@users.noreply.github.com>
2024-02-09 22:35:57 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 10 ms, Memory: 13,536 KB -BaekjoonHub

--- SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.cpp
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.cpp
@@ -6,11 +6,10 @@ int N; //수 갯수
 int arr[12]; //수 입력
 int oper[4]; //operator + - * / 각각 갯수
 int ans_min, ans_max;//답의 최소, 최대값
-int all_oper_count; //모든 연산의 총합
 void dfs(int total, int cnt) { //연산 결과, 현재까지 사용한 연산자 수
 
     //모든 연산자 다 사용한 경우
-    if (cnt == all_oper_count) {
+    if (cnt == N-1) {
         ans_min = min(ans_min, total);
         ans_max = max(ans_max, total);
         return;
@@ -54,13 +53,11 @@ int main(void) {
         //초기화
         ans_min = max_val;
         ans_max = min_val;
-        all_oper_count = 0;
 
         //input
         cin >> N;
         for (int i = 0 ; i < 4; ++i) {
             cin >> oper[i];
-            all_oper_count+= oper[i];
         }
         for (int i = 0 ; i < N; ++i) cin >> arr[i];
 
