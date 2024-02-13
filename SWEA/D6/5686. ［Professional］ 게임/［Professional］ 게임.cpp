81467a3d7ed80e94d619768de9790983620ef7a9
SnowySparks <156272562+SnowySparks@users.noreply.github.com>
2024-02-13 23:20:02 +0900

    [D6] Title: [Professional] 게임, Time: 208 ms, Memory: 15,096 KB -BaekjoonHub

--- SWEA/D6/5686. ［Professional］ 게임/［Professional］ 게임.cpp
+++ SWEA/D6/5686. ［Professional］ 게임/［Professional］ 게임.cpp
@@ -4,7 +4,7 @@ using namespace std;
 const int inf = 1e5+2;
 int sarr[inf];
 int tmp[inf];
-int tmp2[inf][2];
+int tmp2[inf];
 int n, m;
 int dp[inf];
 
@@ -16,8 +16,8 @@ int main(void) {
         //초기화
         memset(dp,0,sizeof(dp));
         memset(sarr,0,sizeof(sarr));
-        memset(sarr,0,sizeof(tmp));
-        memset(tmp2,0,sizeof(tmp2));
+        memset(tmp,0,sizeof(tmp));
+        memset(tmp2, 0, sizeof(tmp2));
         //input
         cin >> n >> m;
         for (int i = 0 ; i < n ; ++i) {
@@ -27,12 +27,10 @@ int main(void) {
 
             //한 줄(가로줄)에 대해서 선택 가능한 경우의 수중 가장 최대값을 도출
             //DP이용 , tmp[col][opt] opt : 0 -> 해당 값을 선택하지 않은 경우, opt : 1-> 해당 값을 선택한 경우
-            tmp2[m-1][1] = tmp[m-1];
-            
+            tmp2[m-1]= tmp[m-1];
             for (int j = m-1; j >=0; --j) {
-                tmp2[j][0] = max(tmp2[j+1][0],tmp2[j+1][1]);//선택 안하는 경우 -> 바로 직전걸 가져옴
-                tmp2[j][1] = max(tmp2[j+2][0],tmp2[j+2][1]) + tmp[j];//선택한 경우 -> 두칸 건너뛴 것들중 최대값 + 자기자신
-                sarr[i] = max(sarr[i],max(tmp2[j][0],tmp2[j][1]));//해당 행에 대한 최대값 갱신
+                tmp2[j] = max(tmp2[j+1], tmp2[j+2] + tmp[j]);
+                sarr[i] = max(sarr[i],tmp2[j]);//해당 행에 대한 최대값 갱신
             }
         }
         //행 방향에 했던 방식 드개로 열 방식으로도 동일하게 적용
