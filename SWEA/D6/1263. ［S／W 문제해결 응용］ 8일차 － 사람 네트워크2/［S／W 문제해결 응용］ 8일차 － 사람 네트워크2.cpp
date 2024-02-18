e797b5231707e20ec9772b23824c22dfa7c303ec
SnowySparks <156272562+SnowySparks@users.noreply.github.com>
2024-02-18 14:01:19 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 395 ms, Memory: 16,088 KB -BaekjoonHub

--- SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.cpp
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.cpp
@@ -8,8 +8,37 @@ using namespace std;
 #define MAX_VAL 1000
 #define f(a,b,c) for (int a=b;a<c;++a)
 vector<int> adj[MAX_VAL+1];
-int dp[1000][1000];
+int dist[MAX_VAL+1]; 
 int n;
+int ans;
+void adj_reset(int n) {
+    f(i, 1, n+1) {
+        adj[i].clear();
+    }
+    ans = INF;
+}
+
+int bfs(int x) {//x : 시작노드
+    memset(dist, -1, sizeof(dist));
+    dist[x] = 0;
+    queue<int> q;
+    q.push(x);
+    int cnt = 0;
+    while (!q.empty()) {
+        int loc = q.front(); 
+        q.pop();
+
+        for (int &a : adj[loc]) {
+            if (dist[a] != -1) continue;
+            dist[a] = dist[loc] + 1;
+            q.push(a);
+            cnt += dist[a];
+            if (cnt > ans) return ans; //최적화 - 작업중 현재 ans보다 큰 경우 지체없이 종료
+        }
+    }
+    return ans = cnt;
+}
+
 
 int main(void) {
     ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
@@ -17,34 +46,17 @@ int main(void) {
     int T; cin >> T;
     int tmp;
     for (int tc = 1; tc <=T;++tc) {
-        cin >> n;
-        f(r, 0, n) {
-            f(c, 0, n) {
-                cin >> dp[r][c];
-                if (r == c) continue;
-                else if (dp[r][c] == 0) dp[r][c] = INF;
-            }
-        }
-        
-        //플루이드 워셜
-        f(mid, 0, n) {
-            f(start, 0, n) {
-                f(end, 0, n) {
-                    if (mid == start || start == end || mid == end) continue;
-                    int mid_pas = dp[start][mid] + dp[mid][end];
-                    if (mid_pas < dp[start][end]) {
-                        dp[start][end] = mid_pas;
-                        dp[end][start] = mid_pas;
-                    }
-                }
-            }
-        }
-
         int ans = INF;
-        f(r, 0, n) {
-            int cnt = 0;
-            f(c, 0, n) cnt+=dp[r][c];
-            ans = min(ans, cnt);
+        cin >> n;
+        adj_reset(n);
+        f(r, 1, n+1) {
+            f(c, 1, n+1) {
+                cin >> tmp;
+                if (tmp) adj[r].push_back(c);
+            }
+        }
+        f(i, 1, n+1) {
+            ans = bfs(i);
         }
         cout << '#' <<tc <<' ' << ans << '\n';
     }
