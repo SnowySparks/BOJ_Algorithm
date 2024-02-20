3d7bcd7e5bffb9ece306687b55932a4a2bec20f4
SnowySparks <156272562+SnowySparks@users.noreply.github.com>
2024-02-20 11:49:05 +0900

    [D3] Title: 퍼펙트 셔플, Time: 150 ms, Memory: 59,968 KB -BaekjoonHub

--- SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.py
+++ SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.py
@@ -1,13 +1,17 @@
 T = int(input())
-for tc in range(1, T+1):
-    n = int(input())
-    lst = input().rstrip().split()
-    if n%2 : lst.append('0')
-    ans = []
-    for i in range((n+1)//2):
-        ans.append(lst[i])
-        ans.append(lst[i + (n+1)//2])
-    if n%2:
-        ans.pop()
-    
-    print(f'#{tc}', *ans)
+for tc in range(1, T + 1):
+    N = int(input())
+    card_lst = list(input().split())
+    if (N % 2) == 1:
+        left_lst = card_lst[:N//2+1]
+        right_lst = card_lst[N//2+1:]
+    else:
+        left_lst = card_lst[:N//2]
+        right_lst = card_lst[N//2:]
+    if (N % 2) == 1:
+        right_lst += ['']
+    new_card_lst = list(zip(left_lst, right_lst))
+    print(f'#{tc} ', end='')
+    for new_card in new_card_lst:
+        print(*new_card, end=' ')
+    print()
