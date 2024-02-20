05d11a2d6322429a0c4dc1f68b6247808dce8662
SnowySparks <156272562+SnowySparks@users.noreply.github.com>
2024-02-20 10:24:17 +0900

    [D3] Title: 정곤이의 단조 증가하는 수, Time: 2,095 ms, Memory: 230,804 KB -BaekjoonHub

--- SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.py
+++ SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.py
@@ -1,26 +1,28 @@
-# import sys
-# sys.stdin = open('input.txt','r')
-
+# 테스트 케이스 T 입력
 T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    num_lst = list(map(int, input().split()))
+    multiply_lst = []
+    check_lst = []
+    for i in range(len(num_lst) - 1):
+        for j in range(i+1, len(num_lst)):
+            multiply_lst.append(num_lst[i] * num_lst[j])
 
-def isrising(n):
-    if n < 10:
-        return True
-    v = n%10
-    while n:
-        nv = n%10
-        if v < nv:
-            return False
-        v = min(v, nv)
-        n//=10
-    return True
-
-for test_case in range(1, T + 1):
-    n = int(input())
-    lst = list(map(int,input().split()))
-    ans = 0
-    for i in range(n-1):
-        for k in range(i+1,n):
-            if isrising(lst[i]*lst[k]):
-                ans = max(ans, lst[i]*lst[k])
-    print(f'#{test_case} {ans if ans > 0 else -1}')
+    multiply_lst = list(map(str, multiply_lst))
+    for mul_num in multiply_lst:
+        check = 0
+        for i in range(len(mul_num) - 1):
+            if int(mul_num[i]) <= int(mul_num[i+1]):
+                continue
+            else:
+                check = 1
+                break
+        if check:
+            continue
+        else:
+            check_lst.append(int(mul_num))
+    if len(check_lst):
+        print(f'#{tc} {max(check_lst)}')
+    else:
+        print(f'#{tc} -1')
