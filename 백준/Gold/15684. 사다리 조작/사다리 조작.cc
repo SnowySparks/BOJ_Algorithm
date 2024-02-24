ae83f34a69870cba4c4b4a22182d6554dfae2e44
SnowySparks <156272562+SnowySparks@users.noreply.github.com>
2024-02-24 22:38:36 +0900

    [Gold III] Title: 사다리 조작, Time: 456 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.cc
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.cc
@@ -1,69 +1,61 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cstring>
 using namespace std;
-#define f(a,b,c) for(int a = b; a < c ; ++a)
+#define f(a,b,c) for(int a=b;a<c;++a)
+int N, M, H;
 
-int n, m, h; //세로선 갯수(열 갯수), 가로선 입력갯수, 사다리 높이
-bool gh[12][32]; // [a][b] -> 높이 a, 세로선 번호가 b에서 true면 해당 위치는 다음
-int ans = 4; //3이하 아니면 -1이나 마찬가지..
-bool fd =false;
-int simulation(int x) { //x 위치 시작 
-    int now_h = 1;
-    while (now_h <= h) {
-        if (gh[x][now_h]) ++x;
-        else if (gh[x-1][now_h]) --x;
-        ++now_h;
+int ans = 4;
+bool ladder[32][12]; //가로줄 위치값, 세로줄 위치값, [a][b] = True이건 해당 a위치에 b, b+1 세로줄에 연결되어있음 의미
+bool fd = false;
+
+int go(int x) { //시작점이 x일때 끝점이 어디지?
+    int nowh = 1;
+    while (nowh <= H)
+    {
+        if (ladder[nowh][x]) ++x;
+        else if (ladder[nowh][x-1]) --x;
+        ++nowh;
     }
     return x;
 }
 
-//모든 세로선에 대해서 자기자신으로 돌아가는지 테스트하는 함수
-bool test() {
-    for (int i = 1; i <= n; ++i) {
-        if (simulation(i) != i) return false;
+bool check() { //전체 탐색하고 하나라도 원래 자리 안가면 False
+    f(i, 1, N+1) {
+        if (go(i) != i) return false;
     }
     return true;
 }
 
-//추가 설치한 갯수, 최대 설치 갯수, 세로선 탐색 시작위치
-void back_track(int total, int max_choice, int start_l, int start_h) {
+void back_track(int nowh, int nowl, int cnt, int maxcnt) { //backtracking
     if (fd) return;
-    if (total == max_choice) {
-        if (test()) {
+    if (maxcnt == cnt) {
+        if (check()) {
+            ans = cnt;
             fd = true;
-            ans = total;
         }
         return;
     }
-    if (fd) return;
-
-//어차피 같은곳 2개 이상 설치하는 것이 의미가 없음
-    for (int h_loc = start_h; h_loc <= h; ++h_loc) {
-        int left_loc = (h_loc == start_h ? start_l : 1);
-        for (;left_loc < n; ++ left_loc) {
-            if (gh[left_loc-1][h_loc] || gh[left_loc][h_loc]  || gh[left_loc+1][h_loc] ) continue;
-            gh[left_loc][h_loc] = true;
-            back_track(total+1, max_choice, left_loc+1, h_loc);
+    f(h, nowh, H+1) {
+        int locl= (h == nowh ? nowl : 1);
+        f(l, locl, N) {
+            if (ladder[h][l-1] || ladder[h][l] || ladder[h][l+1]) continue;
+            ladder[h][l] = true;
+            back_track(h, l+1, cnt+1, maxcnt);
             if (fd) return;
-            gh[left_loc][h_loc] = false;
+            ladder[h][l] = false;
         }
     }
     return;
 }
 
-int main(void) {
+int main(void) { 
     ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    cin >> n >> m >> h;
-    for (int i = 1; i <= n; ++i) gh[i][h+1] = i; //가장밑바닥 설정
-    for (int i = 1; i <= m ; ++i ) {
-        int a, b; cin >> a >> b; //높이위치, 왼쪽 
-        gh[b][a] = true;
+    cin >> N >> M >> H;
+    f(i, 0, M) {
+        int a, b; cin >> a >> b;
+        ladder[a][b] = true;
     }
-    for (int i = 0; i <= 3; ++i) {
-        back_track(0, i, 1, 1);
-    }
-    cout <<(ans > 3 ? -1 : ans);
+    f(i, 0, 4) back_track(1, 1, 0, i);
+    cout << (ans > 3 ? -1 : ans);
     return 0;
+
 }
