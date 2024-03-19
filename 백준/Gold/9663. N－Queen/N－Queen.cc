4b2de4abad3390506732c1d73ca68ea326b7d95b
SnowySparks <156272562+SnowySparks@users.noreply.github.com>
2024-03-19 13:55:22 +0900

    [Gold IV] Title: N-Queen, Time: 1652 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -3,31 +3,34 @@
 #include <algorithm>
 using namespace std;
 
-int col[15]; // col[i]  == (col[i] , i)
+bool row[16];
+bool leftup[31];
+bool rightup[31];
 int total = 0;
-
-bool isdialogal(int level) {
-    for (int i = 0 ; i < level ; i++) {
-        //같은 행에 있거나, 대각선 위치에 있을 경우는 제외 ( 행 차이 크기 == 열 차이 크기 -> 대각선)
-        if (col[i] == col[level] || abs(col[level] - col[i]) == level - i) {
-            return false;    
-        }
+int n;
+void dfs(int col) {
+    if (col == n) {
+        ++total;
+        return;
     }
-    return true;
-}
 
-void dfs(int x, int N) { // x번째 배치 , 최대 N개
-    if (x == N) {total++; return;}
-    for (int i = 0 ; i < N; i++) {
-        //x 열에 일단 배치를 하고, 검사를 해서 맞는 조건인 경우 dfs시행
-        col[x] = i;
-        if (isdialogal(x)) {dfs(x+1,N);}
+    for (int r = 0; r < n; ++r) {
+        int ru = r + col, lu = (n - 1) - (r - col);
+        if (row[r]) continue;
+        if (rightup[ru] || leftup[lu]) continue;
+        row[r] = true;
+        leftup[lu] = true;
+        rightup[ru] = true;
+        dfs(col + 1);
+        row[r] = false;
+        leftup[lu] = false;
+        rightup[ru] = false;        
     }
 }
 
 int main(void) {
-    int N ; cin >> N;
-    dfs(0,N);
-    cout << total;
+    scanf("%d", &n);
+    dfs(0);
+    printf("%d", total);
     return 0;
 }
