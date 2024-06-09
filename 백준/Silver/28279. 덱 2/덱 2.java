f73e6c52d1afe14e2a61315d84fa3c0a27cb6a08
SnowySparks <156272562+SnowySparks@users.noreply.github.com>
2024-06-09 20:58:04 +0900

    [Silver IV] Title: 덱 2, Time: 1224 ms, Memory: 249392 KB -BaekjoonHub

---
+++ 백준/Silver/28279. 덱 2/덱 2.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Input Setting
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder pr = new StringBuilder();
+        // 자료구조
+        Deque<Integer> dq = new ArrayDeque<>();
+        int cmd;
+        // 시행
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; ++i) {
+            st = new StringTokenizer(br.readLine());
+            cmd = Integer.parseInt(st.nextToken());
+
+            switch (cmd) {
+                case 1:
+                    dq.addFirst(Integer.parseInt(st.nextToken()));
+                    break;
+                case 2:
+                    dq.addLast(Integer.parseInt(st.nextToken()));
+                    break;
+                case 3:
+                    pr.append( dq.isEmpty() ? -1 : dq.pollFirst()).append("\n");
+                    break;
+                case 4:
+                    pr.append( dq.isEmpty() ? -1 : dq.pollLast()).append("\n");
+                    break;
+                case 5:
+                    pr.append(dq.size()).append("\n");
+                    break;
+                case 6:
+                    pr.append( dq.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case 7:
+                    pr.append(dq.isEmpty() ? -1 : dq.peekFirst()).append("\n");
+                    break;
+                case 8:
+                    pr.append(dq.isEmpty() ? -1 : dq.peekLast()).append("\n");
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println(pr.toString());
+        br.close();
+    }
+}
