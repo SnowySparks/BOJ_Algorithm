e63ba09a8f27d6eec5ff8906420ae418a260ee20
SnowySparks <156272562+SnowySparks@users.noreply.github.com>
2024-06-12 15:59:02 +0900

    [D3] Title: XY 문자열 1, Time: 125 ms, Memory: 13,540 KB -BaekjoonHub

--- SWEA/D3/20955. XY 문자열 1/XY 문자열 1.cpp
+++ SWEA/D3/20955. XY 문자열 1/XY 문자열 1.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main(void) {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     int t; cin >> t;
     string s, e;
     for (int tc = 1; tc <= t; ++tc) {
